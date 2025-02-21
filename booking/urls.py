241571f5988677d346d140173413d649018c03a0
MiaTothova <21tothova@gmail.com>
2025-02-21 07:11:30 +0000

    Add menu styling, create menu view and urls.

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Index
+from .views import Index, Menu
 
 
 urlpatterns = [
-    path('', Index.as_view(), name='home')
+    path('', Index.as_view(), name='home'),
+    path('menu/', Menu.as_view(), name='menu'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = 'booking/index.html'
+
+
+class Menu(TemplateView):
+    template_name = 'booking/menu.html'
