bcaff2a73a2fd31f6235dc66c3817b0ca863796f
MiaTothova <21tothova@gmail.com>
2025-02-24 16:09:50 +0000

    Create booking form view

--- booking/urls.py
+++ booking/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import Index, Menu, Create
+from .views import Index, Menu, Create, book_table
 
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
     path('menu/', Menu.as_view(), name='menu'),
-    path('create/', Create.as_view(), name='create'),
+    path('create/', book_table, name='create'),
 
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,22 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import BookingForm
+
+
+def book_table(request):
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user
+            booking.save()
+            return redirect('home')
+            messages.success(request,
+                             'Your Booking has been succesful!')
+    else:
+        form = BookingForm()
+    return render(request, 'booking/create.html', {'form': form})
 
 
 class Index(TemplateView):
