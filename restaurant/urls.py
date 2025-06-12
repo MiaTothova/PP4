0fa41905c8a6a2cd76ac01cbeb41cd1d85489463
MiaTothova <21tothova@gmail.com>
2025-06-12 10:26:02 +0100

    Replace boothstrap alert in delete booing to prevent auto-dismiss. remove commented out code from urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from booking.views import my_booking
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    # path('booking/', my_booking, name='booking'),
     path('', include('booking.urls')),
 ]
